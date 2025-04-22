1a7269809be643e9109827b90c95feba92476a3e
Noah Melton <nmelton3@ut.utm.edu>
2025-04-22 16:15:16 -0500

    more done

--- deque.h
+++ deque.h
@@ -1,3 +1,12 @@
+/**
+ * @file deque.h
+ * @author Noah Melton
+ * @date 2025-04-22
+ * @brief this is the header file for the deque class
+ * 
+ * this is where we declare the functions which the deque class will use
+ */
+
 #ifndef DEQUE_H
 #define DEQUE_H
 
@@ -5,6 +14,14 @@
 #include <string>
 
 
+
+/**
+ * the deque class
+ *
+ * @class deque deque.h "Deque/deque.h"
+ * @brief this class is a double ended queue, where we can add and remove items from both the beginning and the end of the queue. 
+ *
+ */
 class deque {
 
 
@@ -14,7 +31,21 @@ class deque {
 
   ~deque();
 
-  push_front
+  void push_front(int number);
+  
+  void push_back(int number);
+
+  void pop_front();
+
+  void pop_back(); 
+
+  void front();
+
+  void back();
+
+  bool empty();
+
+  int size();
 
 
 
