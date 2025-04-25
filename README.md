03b881d73c61a5b9b5382f9b2a46c860a77530ef
Noah Melton <nmelton3@ut.utm.edu>
2025-04-25 16:29:48 -0500

    comments and readme done

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Deque
 This is the repo for the deque group assignment. 
+
+
+What is a deque?
+
+Deque stands for a double ended queue. Essentially, it is a 2D array which stores values. There is an STL deque object, but for this assignment, we were tasked with making our own version. Our version is pretty much the same, as it allows access to the front and end of the deque, just like the STL one does. Howver, the STL deque has a built in [] function which allows element access. Ours does too, but we had to make it. We don't have more niche STL functions, like the clear() function, but for the most part our assignmnet copies the main behaviors of the STL deque. In the main.cpp, we have included some test cases to demonstrate these funcitonalities.
+
+Essentially, what we have is a 2d array (but the user doesn't know that) which just acts like (to the user's eyes) a big one-d array. We can add elements, and delete elements, and access random elements using the [] operator. In addition to this, we can check to see if the deque is empty or if it has stuff inside of it, as well as check to see how long the deque is. 
+
+This assignment was a collab between Noah Melton and Sam Toney. Melton started off by making the .h and .cpp files, and making the constructor/destructor and print function. Toney worked on some other functions and then merged with Melton. Melton then went through and added documentation for everything that didn't already have it and worked on this README file.
+
+Notes: I had a bug that Dr. Bradley helped me sort out with the destructor, and I also consulted Caleb Curry and CSExplained's youtube channels for information about deques and some 2d dynamic array review.
+Finally, I had an issue merging and pulling Sam's commits, so I consulted chatgpt to figure out how to do that so we would be on the same version of the shared code. 
--- deque.h
+++ deque.h
@@ -84,24 +84,117 @@ col = (first_element + i ) % blocksize;
  */
   ~deque();
 
+
+/**
+ * this is the push front function
+ *
+ * @param const int& number the number we are tyring to push to the front of the deque
+ * @pre you need an deque object to be able to push into
+ * @return void the item is pushed into the deque, but nothing is returned
+ * @post the item is inserted to the front of the dequ
+ * 
+ */
   void push_front(const int& number);
   
+
+/**
+ * this is the push back function
+ *
+ * @param const int& number the number we are pushing to the back of the deque
+ * @pre you need a deque to be able to push the number into 
+ * @return void no return, the number is just put onto the end of the deque
+ * @post the item is added to the end of the deque
+ * 
+ */
   void push_back(const int& number);
 
+
+/**
+ * this is the pop front function, which removes the first deque element
+ *
+ * @pre a deque object is needed, and it has to have things inside of it for this to work
+ * @return void the first  element is removed from the deque
+ * @post the first element gets removed, but nothing else changes
+ * 
+ */
   void pop_front();
 
+
+/**
+ * this is the pop back function 
+ *
+ * @pre this needs a deque with things stored inside of ti
+ * @return void no return type, the last element is removed--that's all
+ * @post the last element is removed from the deque
+ * 
+ */
   void pop_back(); 
 
+
+/**
+ * this is the front finder function 
+ *
+ * @pre this function needs to be able to access a deque object
+ * @return int& the number which resides at the front of the deque
+ * @post the number is returned, but nothing about the deque changes
+ * 
+ */
   int& front();
 
+
+/**
+ * this is the back finder funciton 
+ *
+ * @pre this function needs a deque in order to work 
+ * @return int& this is the number which is stored at the end of the deque
+ * @post the number at the end of the deque is returned, and nothing else changes
+ * 
+ */
   int& back();
 
+
+/**
+ * the empty check funciton 
+ *
+ * @pre N/A
+ * @return bool a true or false value indicating whether or not the deque in question is full or empty 
+ * @post N/A
+ * 
+ */
   bool empty();
 
+
+/**
+ * this is the size of funciton 
+ *
+ * @pre N/A
+ * @return int the size of the deque
+ * @post the deque size is returned, but nothing else changes 
+ * 
+ */
   int size();
 
+
+/**
+ * this is the print function 
+ *
+ * @pre n/A
+ * @return void this funciton just prints out stuff, it doesn't return anything
+ * @post the contents are printed out 
+ * 
+ */
   void print();
   
+
+/**
+ * this is the overloaded [] operator which allows us to access deque elements like we would a normal array
+ *
+ * @param int index the index we want to see
+ * @pre a populated deque 
+ * @return const this returns the element which we are trying to see
+ * @post N/A
+ * 
+ */
   const int& operator[](int index);
   
 };
